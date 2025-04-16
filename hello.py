3a07e3ca8d82404824918158ac507831ad97c1a3
M Shafqat sarwar <khansarwar1@hotmail.com>
2025-04-17 03:38:27 +0500

    short-term history added

--- hello.py
+++ hello.py
@@ -4,17 +4,27 @@ from dotenv import load_dotenv, find_dotenv
 from agents import Agent, RunConfig, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
 
 load_dotenv(find_dotenv())
-
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-provider= AsyncOpenAI(
-    api_key=gemini_api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-)
+if not gemini_api_key:
+    # Option 1: Raise an error immediately
+    raise ValueError("GEMINI_API_KEY environment variable not set or found.")
 
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/", # Still needs verification
+)
+# load_dotenv(find_dotenv())
+
+# gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# provider= AsyncOpenAI(
+#     api_key=gemini_api_key,
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+#)
 # Model:
 model = OpenAIChatCompletionsModel(
-    model="gemini-2.0-flash",
+    model="gemini-1.5-flash",
     openai_client = provider,
 )
 # Cofigrations
@@ -23,29 +33,38 @@ run_config = RunConfig(
     model_provider = provider,
     tracing_disabled = True
 )
-
 # Agent:
 agent1 = Agent(
     instructions="You are a helpful assistant.",
     name="UMT Support Assistant"
 )
-
 # Runner:
 # result = Runner.run_sync(
 #     agent1,
 #     input = "What is capital of Pakistan?",
-#     run_config = run_config,
-# )
+#     run_config = run_config,)
 # print(result.final_output)
+
+@cl.on_chat_start
+async def handle_chat_start():
+    cl.user_session.set("history", [])
+    await cl.Message(content="Hello! I'm the UMT Lahore Support Agent. How can I assist you today?").send()
 
 @cl.on_message
 async def handle_message(message: cl.Message):
+    history = cl.user_session.get("history")
+
+    # Standard interface
+    history.append({"role": "user", "content": message.content})
     result = await Runner.run(
         agent1,
-        input = message.content,
+        input = history,
         run_config = run_config,
     )
+    history.append({"role": "assistant", "content": result.final_output})
+    cl.user_session.set("history", history)
     await cl.Message(content=result.final_output).send()
+
 
 # import os
 # import chainlit as cl
