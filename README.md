b2ea0d1e936c436593c19d539589b40f8b547484
M Shafqat sarwar <khansarwar1@hotmail.com>
2025-04-17 04:40:52 +0500

    streaming added in hello1.py

---
+++ README.md
@@ -0,0 +1,6 @@
+to run for conversational history:
+uv run chainlit run hello.py -w
+
+to run for streaming with conversational history:
+uv run chainlit run hello1.py -w
+
--- hello1.py
+++ hello1.py
@@ -2,6 +2,7 @@ import os
 import chainlit as cl
 from dotenv import load_dotenv, find_dotenv
 from agents import Agent, RunConfig, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
+from openai.types.responses import ResponseTextDeltaEvent
 
 load_dotenv(find_dotenv())
 gemini_api_key = os.getenv("GEMINI_API_KEY")
@@ -30,13 +31,6 @@ agent1 = Agent(
     instructions="You are a helpful assistant.",
     name="UMT Support Assistant"
 )
-# Runner:
-# result = Runner.run_sync(
-#     agent1,
-#     input = "What is capital of Pakistan?",
-#     run_config = run_config,)
-# print(result.final_output)
-
 @cl.on_chat_start
 async def handle_chat_start():
     cl.user_session.set("history", [])
@@ -46,53 +40,20 @@ async def handle_chat_start():
 async def handle_message(message: cl.Message):
     history = cl.user_session.get("history")
 
+    msg= cl.Message(content = "")
+    await msg.send()
+
     # Standard interface
     history.append({"role": "user", "content": message.content})
-    result = await Runner.run(
+    result = Runner.run_streamed(
         agent1,
         input = history,
         run_config = run_config,
     )
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            await msg.stream_token(event.data.delta)
+
     history.append({"role": "assistant", "content": result.final_output})
     cl.user_session.set("history", history)
-    await cl.Message(content=result.final_output).send()
-
-
-# import os
-# import chainlit as cl
-# from dotenv import load_dotenv, find_dotenv
-# from agents import Agent, RunConfig, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
-
-# load_dotenv(find_dotenv())
-
-# gemini_api_key = os.getenv("GEMINI_API_KEY")
-
-# provider = AsyncOpenAI(
-#     api_key=gemini_api_key,
-#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-# )
-
-# model = OpenAIChatCompletionsModel(
-#     model="gemini-2.0-flash",
-#     openai_client=provider,
-# )
-
-# run_config = RunConfig(
-#     model=model,
-#     model_provider=provider,
-#     tracing_disabled=True
-# )
-
-# agent1 = Agent(
-#     instructions="You are a helpful assistant.",
-#     name="UMT Support Assistant"
-# )
-
-# @cl.on_message
-# async def handle_message(message: cl.Message):
-#     result = await Runner.run(
-#         agent1,
-#         input=message.content,
-#         run_config=run_config,
-#     )
-#     await cl.Message(content=result.final_output).send() 
+    # await cl.Message(content=result.final_output).send()
